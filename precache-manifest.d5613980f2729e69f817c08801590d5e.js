6ff7b87151528b493937da9ab2ecd501421ef645
Faizan Ali <faizanali_786@hotmail.co.uk>
2020-01-28 17:28:12 +0000

    Updates

---
+++ precache-manifest.d5613980f2729e69f817c08801590d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497c69754f63ca82107f606c7debc075",
+    "url": "/portfolio-f/index.html"
+  },
+  {
+    "revision": "df6b22614338c23e96bd",
+    "url": "/portfolio-f/static/css/main.4d48f30c.chunk.css"
+  },
+  {
+    "revision": "9a373a0fdc4664bbf992",
+    "url": "/portfolio-f/static/js/2.e1175473.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-f/static/js/2.e1175473.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df6b22614338c23e96bd",
+    "url": "/portfolio-f/static/js/main.33865363.chunk.js"
+  },
+  {
+    "revision": "d6e682f8b64bc8db9d46",
+    "url": "/portfolio-f/static/js/runtime-main.ab373fb1.js"
+  }
+]);
